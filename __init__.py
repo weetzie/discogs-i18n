0a4469c7264e449f1f746a96d9c65b2359a7cd3b
Richard Flosi <rflosi@discogs.com>
2012-01-03 12:51:22 -0800

    adding i18n javascript support

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from babel.support import Translations
+import simplejson
 
 def _get_locales():
     """
@@ -7,7 +8,22 @@ def _get_locales():
     locales = ['en', 'de', 'ja']
     from random import randrange
     i = randrange(len(locales))
-    return [locales[i], 'en']
+    return list(set([locales[i], 'en']))
 
-def get_translations():
-    return Translations.load(dirname='i18n', locales=_get_locales())
+def get_translations(locales=[], domain='messages'):
+    return Translations.load(dirname='i18n', locales=locales or _get_locales(), domain=domain)
+
+def get_translations_json(locales=[]):
+    translations = get_translations(locales, domain='messages_js')
+    keys = translations._catalog.keys()
+    keys.sort()
+    translations_dict = {}
+    for k in keys:
+        v = translations._catalog[k]
+        if type(k) is tuple:
+            if k[0] not in translations_dict:
+                translations_dict[k[0]] = []
+            translations_dict[k[0]].append(v)
+        else:
+            translations_dict[k] = v
+    return simplejson.dumps(translations_dict, ensure_ascii=False, indent=False)
